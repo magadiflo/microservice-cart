bc0b65564edb14b3acef0ed5400f95e78541bb0c
Martín <magadiflo@gmail.com>
2022-10-27 17:14:33 -0500

    Agregación de cliente eureka. Habilitación del microservicio como cliente eureka. Configuración para registarse en eureka

--- src/main/java/com/magadiflo/cart/MicroserviceCartApplication.java
+++ src/main/java/com/magadiflo/cart/MicroserviceCartApplication.java
@@ -2,7 +2,9 @@ package com.magadiflo.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceCartApplication {
 
